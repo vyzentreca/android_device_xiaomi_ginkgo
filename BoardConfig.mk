3895ede9f2fe41bb40ef8d76a98a39453f4521ae
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-23 09:35:08 +0200

    ginkgo: Inherit from common tree
    Change-Id: Icaf0e5cb7dbb8c75b81bf9008433ab51a9b34531

---
+++ BoardConfig.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm6125-common
+include device/xiaomi/sm6125-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/xiaomi/ginkgo
+
+# Display
+TARGET_SCREEN_DENSITY := 440
+
+# Kernel
+TARGET_KERNEL_CONFIG := vendor/ginkgo-perf_defconfig
+
+# Inherit from the proprietary version
+include vendor/xiaomi/ginkgo/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from sm6125-common
+$(call inherit-product, device/xiaomi/sm6125-common/common.mk)
+
+DEVICE_PATH := device/xiaomi/ginkgo
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 28
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/ginkgo/ginkgo-vendor.mk)
